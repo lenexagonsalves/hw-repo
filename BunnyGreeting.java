771afc44fc9d445c4463ba784546adddbb0cb7a9
Lennie Gonsalves <32377970+lenexagonsalves@users.noreply.github.com>
2020-09-09 10:45:51 -0700

    Upload BunnyGreeting.java
    Created bunny ears for homework 1.

---
+++ BunnyGreeting.java
@@ -0,0 +1,16 @@
+//BunnyGreeting.java is a simple class that executes an rabbit animal greeting
+import java.util.*;
+public class BunnyGreeting{
+
+    public static void main (String[] args){
+        System.out.println("        / \\                                                 ");
+        System.out.println("       /  _\\                                               ");
+        System.out.println("       | / \\|                                               ");
+        System.out.println("       ||   || _______                                       ");
+        System.out.println("       ||   || |\\     \\                                    ");
+        System.out.println("       ||   || ||\\     \\                                   ");
+        System.out.println("       ||   || || \\     |                                   ");
+
+        }
+
+}
