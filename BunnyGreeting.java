fac97a038150ccb770ba183ed761dc9f1f3d982f
Lennie Gonsalves <32377970+lenexagonsalves@users.noreply.github.com>
2020-09-09 11:02:02 -0700

    Update BunnyGreeting.java
    File now includes the rest of the bunny and the greeting

--- BunnyGreeting.java
+++ BunnyGreeting.java
@@ -4,12 +4,26 @@ public class BunnyGreeting{
 
     public static void main (String[] args){
         System.out.println("        / \\                                                 ");
-        System.out.println("       /  _\\                                               ");
+        System.out.println("       /  _\\                                                ");
         System.out.println("       | / \\|                                               ");
         System.out.println("       ||   || _______                                       ");
         System.out.println("       ||   || |\\     \\                                    ");
         System.out.println("       ||   || ||\\     \\                                   ");
         System.out.println("       ||   || || \\     |                                   ");
+        System.out.println("       ||   || ||  \\__ /                                    ");
+        System.out.println("       ||   || ||   ||                                       ");
+        System.out.println("        \\\\_/ \\_/ \\_//                                    ");
+        System.out.println("       /   _     _   \\                                      ");
+        System.out.println("      /               \\                                     ");
+        System.out.println("      |    O     O    |                                      ");
+        System.out.println("      |   \\  ___  /   |                                     ");
+        System.out.println("     /     \\ \\_/ /     \\                                  ");
+        System.out.println("    /  -----  |  --\\    \\          ____________________                          ");
+        System.out.println("   |      \\__/|\\__/ \\   |       /                       \\                      ");
+        System.out.println("    \\       |_|_|       /     <         Hey there!       |                        ");
+        System.out.println("      \\_____      _____/        \\                        /                       ");
+        System.out.println("           \\     /                 --------------------                           ");
+        System.out.println("           |     |                                                                 ");
 
         }
 
