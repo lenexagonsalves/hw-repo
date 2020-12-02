5fb14e8fb009be8a041c7b7eda5849f0b9d67e9e
Lennie Gonsalves <32377970+lenexagonsalves@users.noreply.github.com>
2020-12-02 12:16:15 -0800

    Create Pair<T>.java

---
+++ Pair<T>.java
@@ -0,0 +1,11 @@
+public class Pair<T> 
+{  
+        public T firstElement;
+        public T secondElement;
+        
+        public Pair(T firstElement, T secondElement) 
+        {
+                this.firstElement = firstElement;
+                this.secondElement = secondElement;
+        }
+}
